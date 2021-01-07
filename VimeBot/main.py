38a513b28ed4805ef667cc537705fcdb80d768fc
lamer <mangushev.maks13@mail.ru>
2021-01-07 20:17:07 +0000

    Обновление точки входа

--- VimeBot/main.py
+++ VimeBot/main.py
@@ -7,10 +7,13 @@ def main():
     client = commands.Bot(command_prefix=None)
     client.remove_command("help")
 
-    for module in ...:
+    with open("config.json", "r", encoding="utf-8") as file:
+        config = json.load(file)
+
+    for module in config["Components"]:
         for filename in os.listdir(f"assets/{module}"):
             if filename.endswith(".py"):
                 client.load_extension(f"assets.{module}.{filename[:-3]}")
 
-    token = ...
+    token = config["token"]
     client.run(token)
