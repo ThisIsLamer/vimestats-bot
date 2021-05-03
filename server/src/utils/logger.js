9cfabbff8e0a672adf3f10d30d124ae3dab3598e
Lamer <mangushev.maks13@mail.ru>
2021-05-03 14:46:32 +0300

    Updating the logger, now it can write logs to a file in the corresponding folder (logs)

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
+
+// создаём log файл по дате
+const date = new Date().toLocaleString().replace(":", "-").replace(":", "-");
+const filename = process.cwd() + `/logs/${date}.log`;
+fs.open(filename, "a", err => {if (err) console.error(err)});
+
 // формирование текста для вывода по заданным параметрам
 function preparation(lvl, text, settings) {
     let level, date, module, body;
@@ -11,12 +18,16 @@ function preparation(lvl, text, settings) {
 }
 
 // параметры вывода логов в консоль
-function consoleOutput(lvl, text, settings) {
+function console_output(lvl, text, settings) {
     // разукрашиваем текст в консоли
     if (lvl === "debug" || lvl === "log") console.log(preparation(lvl, text, settings), "\x1b[0m")      // стандартный
     if (lvl === "error") console.log("\x1b[32m", preparation(lvl, text, settings), "\x1b[0m");           // зелёный
     if (lvl === "warn") console.log("\x1b[33m", preparation(lvl, text, settings), "\x1b[0m");            // жёлтый
     if (lvl === "fatal") console.log("\x1b[31m",preparation(lvl, text, settings), "\x1b[0m");            // красный
+}
+
+function writing_to_file(lvl, text, settings, recording) {
+    if (recording) fs.appendFile(filename, preparation(lvl, text, settings) + "\n", err => {if (err) console.error(err)});
 }
 
 module.exports = {
@@ -26,14 +37,24 @@ module.exports = {
     settings: {level: true, date: true, module: true, body: true, file: true, console: true},
 
     // уровни логирования
-    debug(value) {if (this.settings.console) consoleOutput("debug", value, this.settings)},
-    log(value) {if (this.settings.console) consoleOutput("log", value, this.settings)},
-    error(value) {if (this.settings.console) consoleOutput("error", value, this.settings)},
-    warn(value) {if (this.settings.console) consoleOutput("warn", value, this.settings)},
-    fatal(value) {if (this.settings.console) consoleOutput("fatal", value, this.settings)},
-
-    // параметры записи логов вв файл
-    writingFile() {
-        
-    },
+    debug(value, recording=true) {if (this.settings.console) {
+        console_output("debug", value, this.settings);
+        writing_to_file("debug", value, this.settings, recording);
+    }},
+    log(value, recording=true) {if (this.settings.console) {
+        console_output("log", value, this.settings);
+        writing_to_file("log", value, this.settings, recording);
+    }},
+    error(value, recording=true) {if (this.settings.console) {
+        console_output("error", value, this.settings);
+        writing_to_file("error", value, this.settings, recording);
+    }},
+    warn(value, recording=true) {if (this.settings.console) {
+        console_output("warn", value, this.settings);
+        writing_to_file("warn", value, this.settings, recording);
+    }},
+    fatal(value, recording=true) {if (this.settings.console) {
+        console_output("fatal", value, this.settings);
+        writing_to_file("fatal", value, this.settings, recording);
+    }},
 }
